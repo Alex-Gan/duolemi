ca42614fa64b5efc87297c1d85003aad9abf631d
ganchaosheng <ganchaosheng@c4297c29-83b5-46f7-973e-9d4be67dcea6>
2019-09-12 12:30:06 +0000

    微信配置参数提交

---
+++ config/wx.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    //  +---------------------------------
+    //  微信相关配置
+    //  +---------------------------------
+
+    // 小程序app_id
+    'app_id' => 'wx0224c4cf54200030',
+    // 小程序app_secret
+    'app_secret' => 'e7bcc52aceced0be67443a2696193262',
+
+    // 微信使用code换取用户openid及session_key的url地址
+    'login_url' => "https://api.weixin.qq.com/sns/jscode2session?" .
+        "appid=%s&secret=%s&js_code=%s&grant_type=authorization_code",
+
+    // 微信获取access_token的url地址
+    'access_token_url' => "https://api.weixin.qq.com/cgi-bin/token?" .
+        "grant_type=client_credential&appid=%s&secret=%s",
+
+    'expires_in' => 7200,
+
+];
