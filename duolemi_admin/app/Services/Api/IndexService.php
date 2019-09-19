f8783aa015b37a7932ee361f44897e599b578ec4
ganchaosheng <ganchaosheng@c4297c29-83b5-46f7-973e-9d4be67dcea6>
2019-09-19 03:40:13 +0000

    体验课详情页

--- duolemi_admin/app/Services/Api/IndexService.php
+++ duolemi_admin/app/Services/Api/IndexService.php
@@ -150,10 +150,29 @@ class IndexService extends BaseService
         }
 
         /*体验课信息*/
-        $experience_course = ExperienceCourse::select(['id', 'name as title', 'experience_price as freePrice', 'original_price as price', 'experience_price as totalCount'])
+        $experience_course = ExperienceCourse::select(['id', 'name as title', 'experience_price as freePrice', 'original_price as price', 'banner', 'details as content', 'experience_price as totalCount'])
             ->where('id', $id)
             ->where('is_delete', 0)
             ->first();
+
+        if (empty($experience_course)) {
+            return $this->formatResponse(404, '体验课信息不存在');
+        }
+
+        /*banner图格式转化*/
+        $banner_arr = json_decode($experience_course->banner, true);
+        $banner_arr_new = [];
+        foreach ($banner_arr as $banner) {
+            $banner_arr_new[] = $banner['img'];
+        }
+        $experience_course->banner = $banner_arr_new;
+
+        /*将详情简介内容格式化*/
+        $experience_course->content = htmlspecialchars_decode($experience_course->content);
+
+        /*客服电话*/
+        $contact = NavigationSettings::where('id', 4)->value('type_relation');
+        $experience_course->contact = !empty($contact) ? $contact : '40012345678';
 
         return $this->formatResponse(0, 'ok', $experience_course);
     }
