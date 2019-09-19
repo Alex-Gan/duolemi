bf5c14b8ae23c42d34ec3757bea383ced8c08370
ganchaosheng <ganchaosheng@c4297c29-83b5-46f7-973e-9d4be67dcea6>
2019-09-19 06:46:08 +0000

    接口

---
+++ duolemi_admin/app/Http/Controllers/Api/LeagueController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Services\Api\LeagueService;
+
+class LeagueController
+{
+    /*定义service变量*/
+    protected $service;
+
+    /**
+     * 加盟课的构造方法
+     *
+     * IndexController constructor.
+     * @param LeagueService $leagueService
+     */
+    public function __construct(LeagueService $leagueService)
+    {
+        $this->service = $leagueService;
+    }
+
+    /**
+     * 加盟课程详情
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function leagueDetail(Request $request)
+    {
+        return $this->service->leagueDetail($request->input());
+    }
+
+    /**
+     * 申请加盟
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function leagueApply(Request $request)
+    {
+        return $this->service->leagueApply($request->input());
+    }
+}
